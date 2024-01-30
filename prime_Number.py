15c13ced1be80f5fbbf9d116ffe16e09a32b52eb
ydeshmane <ydeshmane@evertz.com>
2024-01-31 00:05:47 +0530

    Adding The New Files to the area

---
+++ prime_Number.py
@@ -0,0 +1,23 @@
+# n = 5678
+# isPrime = True
+# i = 2
+
+# for i in range(2, n):
+#     if n % i == 0:
+#         isPrime = False
+#         print("Not Prime")
+#         break
+
+# if isPrime:
+#     print("Prime")
+
+list =["1","Banana",23456]
+print(list)
+list.append("Yogesh")
+print(list)
+
+list.extend(["tushar","om"])
+
+
+
+print(list)
